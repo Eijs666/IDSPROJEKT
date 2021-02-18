b03568f248d680d04ac2aa67e5546457ba2b748d
Eijs666 <53994213+Eijs666@users.noreply.github.com>
2021-02-18 22:06:42 +0100

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,13 +1,13 @@
 package sample.Figure;
-
+//herunder henter vi vores canvas bibliotek
 import javafx.scene.canvas.GraphicsContext;
-
+//vi nedarver fra vores superclass som er figure og den bruger vi til at kunne tegne vores rectangle i vores canvas
 public class Rectangle extends Figure {
     @Override
     public String toString() {
         return "Square";
     }
-
+// getcopy funktionen er den vi nedarver fra vores superclass.
     @Override
     public Figure getCopy() {
         return new Rectangle();
