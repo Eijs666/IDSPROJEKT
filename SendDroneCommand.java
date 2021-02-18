20d8c5494ed1230ad165c9f6978df6d16840e4d2
Eijs666 <53994213+Eijs666@users.noreply.github.com>
2021-02-18 22:16:12 +0100

    Update SendDroneCommand.java

--- SendDroneCommand.java
+++ SendDroneCommand.java
@@ -2,6 +2,7 @@ package sample;
 
 //AN INTERFACE THE OTHER CLASSES CAN CALL UPON
 // vi har lavet en funktion der gør det muligt at få sendt kommandoer til dronen. denne funktion kan bruges i alle klasser
+//interface gør det muligt at vi kan definere en funktionaliteten og ikke at implementere den.
 public interface SendDroneCommand {
     public void SendCommand(String command);
 }
